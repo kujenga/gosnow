b3e84e014fb7b32271c75288ae80e19284d6f73a
kujenga <ataylor0123@gmail.com>
2015-06-20 11:20:35 -0400

    parser with options and circle ci config

--- parser.go
+++ parser.go
@@ -13,19 +13,34 @@ import (
 	"unsafe"
 )
 
-// Parse parses the inputted string and passes it to the drafter library for parsing.
+const (
+	// ScRenderDescriptionsOptionKey causes the parser to render markdown in description
+	ScRenderDescriptionsOptionKey = 1 << 0
+	// RequireBlueprintNameOptionKey causes the parser to treat missing blueprint name as error
+	RequireBlueprintNameOptionKey = 1 << 1
+	// ExportSourcemapOptionKey causes the parser to export source maps AST
+	ExportSourcemapOptionKey = 1 << 2
+)
+
+// OptionParse parses the inputted string and passes it to the drafter library for parsing.
 // The JSON result is returned from the function
-func Parse(source string) (string, error) {
+func OptionParse(source string, flags int) (string, error) {
 	cs := C.CString(source)
 	var result string
 	cr := C.CString(result)
-	ret := int(C.drafter_c_parse(cs, 0, &cr))
+	ret := int(C.drafter_c_parse(cs, C.sc_blueprint_parser_options(flags), &cr))
 	if ret != 0 {
-		return "", fmt.Errorf("drafter_c_parse faile with code: %v", ret)
+		return "", fmt.Errorf("drafter_c_parse failed with code: %v", ret)
 	}
 
 	res := C.GoString(cr)
 	C.free(unsafe.Pointer(cr)) /* we MUST release allocted memory for result */
 
 	return res, nil
+}
+
+// Parse parses the inputted string and passes it to the drafter library for parsing.
+// The JSON result is returned from the function
+func Parse(source string) (string, error) {
+	return OptionParse(source, 0)
 }
--- parser_test.go
+++ parser_test.go
@@ -4,13 +4,29 @@ import (
 	"testing"
 )
 
+const simpleSrc = "# My API\n## GET /message\n + Response 200 (text/plain)\n\n        Hello World\n"
+
 func TestBasicParse(t *testing.T) {
-	src := "# My API\n## GET /message\n + Response 200 (text/plain)\n\n        Hello World\n"
-	res, err := Parse(src)
+	res, err := Parse(simpleSrc)
 	if err != nil {
 		t.Fatalf("Parse failed with error: %v", err)
 	}
 	if res == "" {
 		t.Fatal("Parse returned empty result")
+	}
+}
+
+func TestParseEquality(t *testing.T) {
+	res1, err := Parse(simpleSrc)
+	if err != nil {
+		t.Fatalf("Parse failed with err: %v", err)
+	}
+	res2, err := OptionParse(simpleSrc, 0)
+	if err != nil {
+		t.Fatalf("OptionParse failed with err: %v", err)
+	}
+
+	if res1 != res2 {
+		t.Error("Results should be equal")
 	}
 }
